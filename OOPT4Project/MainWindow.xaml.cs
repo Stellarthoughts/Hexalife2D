b0ffb5adee265795a4d01ee01eeea8f6ae3643f2
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-08 18:28:01 +0700

    fix: Structure

--- OOPT4Project/MainWindow.xaml.cs
+++ OOPT4Project/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OOPTermProject
+namespace OOPT4Project
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml

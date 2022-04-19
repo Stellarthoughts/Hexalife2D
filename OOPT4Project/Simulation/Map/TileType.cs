9090fcce928b705617ffcc85ab158c4189ddc630
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-19 21:39:22 +0700

    feat: TileTypeResources dictionary

--- OOPT4Project/Simulation/Map/TileType.cs
+++ OOPT4Project/Simulation/Map/TileType.cs
@@ -13,6 +13,23 @@ namespace OOPT4Project.Simulation.Map
 		Ocean
 	}
 
+	public struct TileTypeResources
+	{
+		public double InitialFood { get; private set; }
+		public double InitialWater { get; private set; }
+		public double ReplenishRateFood { get; private set; }
+
+		public double ReplenishRateWater { get; private set; }
+
+		public TileTypeResources(double initialFood, double initialWater, double replenishRateFood, double replenishRateWater)
+		{
+			InitialFood = initialFood;
+			InitialWater = initialWater;
+			ReplenishRateFood = replenishRateFood;
+			ReplenishRateWater = replenishRateWater;
+		}
+	}
+
 	public static class TileTypeLogic
 	{
 		public static readonly List<TileType> Types = new() {
@@ -27,13 +44,13 @@ namespace OOPT4Project.Simulation.Map
 
 		public static readonly Dictionary<TileType, double> ProbWeightsDefault = new()
 		{
-			{ TileType.Grass, 1 },
-			{ TileType.Lake, 0.2 },
-			{ TileType.Hills, 0.2 },
-			{ TileType.Desert, 0.2 },
+			{ TileType.Grass,	1 },
+			{ TileType.Lake,	0.2 },
+			{ TileType.Hills,	0.2 },
+			{ TileType.Desert,	0.2 },
 			{ TileType.Badland, 0.3 },
-			{ TileType.Marsh, 0.3 },
-			{ TileType.Ocean, 0 }
+			{ TileType.Marsh,	0.3 },
+			{ TileType.Ocean,	0 }
 		};
 
 		public static readonly Dictionary<TileType, bool> CanWalkTo = new()
@@ -45,6 +62,17 @@ namespace OOPT4Project.Simulation.Map
 			{ TileType.Badland, true },
 			{ TileType.Marsh, true },
 			{ TileType.Ocean, false }
+		};
+
+		public static readonly Dictionary<TileType, TileTypeResources> Resources = new()
+		{
+			{ TileType.Grass,	new TileTypeResources(50, 50, 2, 2) },
+			{ TileType.Lake,    new TileTypeResources(50, 50, 2, 2) },
+			{ TileType.Hills,   new TileTypeResources(50, 50, 2, 2) },
+			{ TileType.Desert,  new TileTypeResources(50, 50, 2, 2) },
+			{ TileType.Badland, new TileTypeResources(50, 50, 2, 2) },
+			{ TileType.Marsh,   new TileTypeResources(50, 50, 2, 2) },
+			{ TileType.Ocean,   new TileTypeResources(50, 50, 2, 2) }
 		};
 	}
 }

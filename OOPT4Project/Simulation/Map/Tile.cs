a9c727cb880c9499dbcd7b0cf42cdb76876fa43e
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-24 23:17:20 +0700

    feat: TileTypeResources to Tile association

--- OOPT4Project/Simulation/Map/Tile.cs
+++ OOPT4Project/Simulation/Map/Tile.cs
@@ -11,14 +11,22 @@ namespace OOPT4Project.Simulation.Map
 		public List<CreatureEntity> CreatureList { get; private set; } = new List<CreatureEntity>();
 		public TileType Type { get; private set; }
 
-		private double _foorResource;
+		private double _foodResource;
 		private double _waterResource;
+
+		private double _foodReplenish;
+		private double _waterReplenish;
 
 		public Tile(Coordinates coor, TileType type)
 		{
 			TileClimate = new TileClimate();
 			Coordinates = coor;
 			Type = type;
+			TileTypeLogic.Resources.TryGetValue(type, out TileTypeResources resources);
+			_foodResource = resources.InitialFood;
+			_waterResource = resources.InitialWater;
+			_foodReplenish = resources.ReplenishRateFood;
+			_waterReplenish = resources.ReplenishRateWater;
 		}
 
 		public void SimulateStep()

571bbfcf4edcad65dcd8603b32db2462ad0ac2c6
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-04-24 23:17:29 +0700

    Create CreatureDrawer.cs

---
+++ OOPT4Project/Render/Creature/CreatureDrawer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPT4Project.Render.Creature
+{
+	internal class CreatureDrawer
+	{
+	}
+}
